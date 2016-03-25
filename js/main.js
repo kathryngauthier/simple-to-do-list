2a594c01a4128ebc35a2e4a8f740a37a263cc3ea
kathryngauthier <Kathryn@Kathryns-MacBook-Pro.local>
2016-03-25 06:20:44 -0400

    finishedl fixed html

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
-var $input = $('#to-do-list');
-var $list = $(".list");
 
-$('#to-do-list').on('submit', function (e) {
+var $input = $('#listinput');
+var $list = $('.list');
+
+$('form').on('submit', function (e) {
   var $li = $('<li>');
+
   e.preventDefault();
   $li.html($input.val());
   $list.append($li);
+  $input.val('');
+});
+
+$('ul').on('click', 'li', function (e) {
+  $(this).toggleClass('todo-input');
 });
